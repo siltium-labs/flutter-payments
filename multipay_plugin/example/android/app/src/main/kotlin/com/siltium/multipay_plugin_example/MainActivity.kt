28ddbd2abf0e0b162908f70f5f789769631e53a6
Jorge Luis Mamani <jmamani@siltium.com>
2023-08-16 16:54:55 -0300

    plugin reset

--- multipay_plugin/example/android/app/src/main/kotlin/com/siltium/multipay_plugin_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.siltium.multipay_plugin_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- multipay_plugin/lib/src/utility/function_utils.dart
+++
@@ -1,8 +0,0 @@
-T? classFromJson<T>(
-    Map<String, dynamic>? json, T Function(Map<String, dynamic>) fromJson) {
-  return json != null ? fromJson(json) : null;
-}
-
-Map<String, dynamic>? classToJson(dynamic object) {
-  return object != null ? object!.toJson() : null;
-}
